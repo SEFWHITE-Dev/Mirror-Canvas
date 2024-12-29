9159f5c4e4af0cb9a2ee9cfbcc99805b40c70cd4
WhiteNoise01 <153906600+SEFWHITE-Dev@users.noreply.github.com>
2024-12-29 23:36:34 +0900

    clerk + convex

---
+++ mirror-canvas-project/convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://moral-hippo-94.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
---
+++ mirror-canvas-project/providers/convex-client-provider.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs"
+import { ConvexProviderWithClerk } from "convex/react-clerk"
+import { 
+  AuthLoading,
+  Authenticated,
+  ConvexProvider,
+  ConvexReactClient,
+ } from "convex/react"
+import { Loading } from "@/components/auth/loading";
+
+
+ interface ConvexClientProviderProps {
+  // kind like a layout
+  // protects all of the children with a layer of authentication
+  children: React.ReactNode; 
+ }
+
+ // ! at the end of a var so it's never Undefined
+ // the property is declared in .env.local
+ const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+ const convex = new ConvexReactClient(convexUrl);
+
+ export const ConvexClientProvider =({
+  children, // destructure the children
+ }: ConvexClientProviderProps) => { // :ObjectType
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Authenticated>
+          {children}
+        </Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+ };
